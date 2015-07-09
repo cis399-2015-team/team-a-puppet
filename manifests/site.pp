c7c1f246955caefd14e58e3b4ee4cbf77e699733
Austin Gheen <austingheen@128-223-223-249-wireless.uoregon.edu>
2015-07-09 16:25:08 -0700

    site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,10 @@
-node ip-10-0-8-251{}
+node ip-10-0-8-251{
+cron {"puppet update":
+	command => "cd /etc/puppet && git pull -q origin master",
+	user => root,
+	minute => "*/5",
+	 }
+}
 
 node ip-10-0-8-128{}
 
