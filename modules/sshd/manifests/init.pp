66057aa7153714e8791b9499559668a617d55647
Austin Gheen <austingheen@Austin-Gheens-MacBook-Pro.local>
2015-07-12 12:05:28 -0700

    init.pp

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -1,31 +1,38 @@
 class sshd
 {
-	package{
+	package
+	{
 		"sshd": ensure => isntalled;
-	 		}
+	 }
 
-	file{"/etc/puppet.conf":
-		source => ["puppet:///modules/sshd/$hostname/sshd_config",
-					"puppet:///modules/sshd/sshd_config", ], 
-		}
-		mode => 444,
-		owner => root,
-		group => root,
+	file
+	{
+		"/etc/puppet.conf":
+		source => 
+		["puppet:///modules/sshd/$hostname/sshd_config",
+		"puppet:///modules/sshd/sshd_config", ], 
+	
 
-		require => Package["sshd"],
- 
-		service
-		{
-			"sshd":
+	mode => 444,
+	owner => root,
+	group => root,
+
+	require => Package["sshd"],
+
+	}
+
+	service
+	{
+		"sshd":
 
 		enable => true,
 		ensure => running,
 		hasstatus => true,
 		hasrestart => true,
 		require => [Package["sshd"],
-					File["/etc/sshd_config"] ],
+		File["/etc/sshd_config"] ],
 
 		subscribe => File["/etc/sshd_config"],
 
-		}
+	}
  }
