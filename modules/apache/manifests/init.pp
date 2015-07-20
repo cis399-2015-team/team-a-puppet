82017b5a405e08004154a9d68bf746bc47a8b9a1
Austin Gheen <austingheen@Austin-Gheens-MacBook-Pro.local>
2015-07-19 17:26:04 -0700

    apache init.pp

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -17,18 +17,6 @@ class apache
 
 	}
 
-	file
-	{
-		"/var/www/html/hello.html":
-		
-	source => ["puppet:///modules/apache/hello.html", ], 
-	mode => 444,
-	owner => root,
-	group => root,
-	require => Package["apache2"],
-
-	}
-
 	service
 	{
 		"apache2":
