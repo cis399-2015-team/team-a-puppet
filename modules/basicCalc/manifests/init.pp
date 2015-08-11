197aacd80b4425c50e9e603da00a58c514255193
Austin Gheen <austingheen@128-223-223-248-wireless.uoregon.edu>
2015-08-11 15:59:57 -0700

    init.pp

--- modules/basicCalc/manifests/init.pp
+++ modules/basicCalc/manifests/init.pp
@@ -21,7 +21,7 @@ class basicCalc
 	{
 		"/var/www/html/calculator.html":
 		
-	source => ["puppet:///modules/BasicCalc/calculator.html", ], 
+	source => ["puppet:///modules/basicCalc/calculator.html", ], 
 	mode => 444,
 	owner => root,
 	group => root,
